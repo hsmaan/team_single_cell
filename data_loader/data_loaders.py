d08d0a84eea3c4b7c9e9955058084718abd64f33
just1ce415 <dimon.ka2002@gmail.com>
2022-12-06 19:34:35 +0200

    Some amendments on data_loaders; added trainers and test from example repository.

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,4 +1,5 @@
-from torchvision import datasets, transforms
+import torch
+import scanpy as sc
 from base import BaseDataLoader
 
 
@@ -16,12 +17,10 @@ from base import BaseDataLoader
         #super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 class GexAtacDataLoader(BaseDataLoader):
-    def __init__(self, gex_adata, atac_adata, batch_size, shuffle=False, validation_split=0.0, num_workers=1, training=True) -> None:
-        gex_arr = gex_adata.X.todense()
-        atac_arr = atac_adata.X.todense()
-        gex_data = 0 # Convert numpy array to torch IterableDataSet ??
-        atac_data = 0 # Convert numpy array to torch IterableDataSet ??
-        super().__init__(gex_adata, atac_adata, batch_size, shuffle, validation_split, num_workers)
+    def __init__(self, directory, batch_size=1) -> None:
+        gex_atac = sc.read_h5ad(directory)
+        gex_atac_tensor = torch.from_numpy(gex_atac.X.todense())
+        super().__init__(gex_atac_tensor, batch_size)
 
 class GexAdtDataLoader(BaseDataLoader):
     pass
