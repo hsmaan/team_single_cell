e4b4d3ae0dafef6f68581d631d41245525d8f850
angelo <dimon.ka2002@gmail.com>
2022-12-11 05:11:55 -0500

    Some adjustments on logging and also demo notebooks for GEX-ADT.

--- evaluate.py
+++ evaluate.py
@@ -21,8 +21,41 @@ class GexAtacEvaluation:
 
         # Loggers
         print()
-        print("AnnData object shape: {};".format(self.adata.shape))
-        print("Latent space shape: {}".format(self.latent_repr.size))
+        print("Latent space type: {};".format(type(self.latent_repr)))
+        print("GEX encoded shape: {};".format(gex_Z.shape))
+        print("ATAC encoded shape: {};".format(atac_Z.shape))
+        print("Latent space shape: {};".format(self.latent_repr.shape))
+        print()
+
+    def evaluate(self):
+        eval_obj = EvalEmbeddings(adata=self.adata) # Initialize the class
+        total_score, res_df = eval_obj.evaluate(self.latent_repr) # Evaluate
+        print(total_score)
+        print(res_df)
+        eval_obj.plot()
+
+class GexAdtEvaluation:
+    def __init__(self, autoencoder, dataset:AnnDataDataset, adata, device) -> None:
+        # Put the model in eval mode and turn off torch gradients to get 
+        # the model embeddings 
+        autoencoder.eval()
+        with torch.no_grad():
+            # Have to move tensors to device and put them in the same dtype or else
+            # this won't work
+            gex_Z = autoencoder.gex_encoder(dataset.get_first_modality().double().to(device))
+            adt_Z = autoencoder.adt_encoder(dataset.get_second_modality().double().to(device))
+            
+        Z_concat = torch.cat([gex_Z, adt_Z], axis=1)
+        Z_concat_np = Z_concat.cpu().detach().numpy()
+        self.latent_repr = Z_concat_np
+        self.adata = adata
+
+        # Loggers
+        print()
+        print("Latent space type: {};".format(type(self.latent_repr)))
+        print("GEX encoded shape: {};".format(gex_Z.shape))
+        print("ADT encoded shape: {};".format(adt_Z.shape))
+        print("Latent space shape: {};".format(self.latent_repr.shape))
         print()
 
     def evaluate(self):
